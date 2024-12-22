e2e060997bb2ec01fae1957eab2dab9ec75e3303
Firzan <firzanruzain@users.noreply.github.com>
2024-12-22 22:44:57 +0800

    update

--- scripts/side-menu.js
+++ scripts/side-menu.js
@@ -1,5 +1,5 @@
 /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("menu-button");
+var dropdown = document.getElementsByClassName("dropdown-button");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
@@ -11,6 +11,20 @@ for (i = 0; i < dropdown.length; i++) {
       dropdownContent.style.display = "none";
     } else {
       dropdownContent.style.display = "block";
+    }
+
+    for (j = 0; j < dropdown.length; j++) {
+      if (this != dropdown[j]) {
+        console.log(dropdown[j]);
+
+        var dropdownContent =
+          dropdown[j].parentNode.querySelector("ul.inner-dropdown");
+        if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+          dropdown[j].parentNode.classList.remove("expanded");
+          dropdownContent.classList.toggle("slideDown");
+        } 
+      }
     }
   });
 }
